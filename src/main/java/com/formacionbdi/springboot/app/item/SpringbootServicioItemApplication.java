980da9fcbf293c6528f7b06669e42f622ac2644a
FelipeVidal <felipevidalsan@gmail.com>
2024-05-22 09:51:34 -0500

    first commit

---
+++ src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
@@ -0,0 +1,20 @@
+package com.formacionbdi.springboot.app.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableDiscoveryClient
+@EnableFeignClients
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+public class SpringbootServicioItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioItemApplication.class, args);
+	}
+
+}
